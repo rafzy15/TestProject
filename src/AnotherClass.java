823ebfd6b4d20f11ee558a6a974d663ab79144c7
rafzy15 <rafzy15@gmail.com>
2017-08-13 16:25:04 +0200

    some changes

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -9,11 +9,10 @@
  * @author rafal
  */
 public class AnotherClass {
-<<<<<<< HEAD
+
 	//another conflict comment
-=======
 	//comment to get conflict
->>>>>>> 521dfc74ed1316d299e7a2e55569ae6528108b6e
+
     public static void main(String[] args) {
         System.out.println("Change to git");
     }
